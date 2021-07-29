c39647229ab77cd1166ca74dfbc055e85b825c80
yosadchyi <yurii.osadchyi@gmail.com>
2021-07-29 10:30:57 +0300

    Publish to npmjs

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secp256k1PublicKey = exports.AzureKeyVaultSecp256k1IdentityOpts = exports.AzureKeyVaultSecp256k1Identity = void 0;
-var identity_1 = require("./identity/identity");
-Object.defineProperty(exports, "AzureKeyVaultSecp256k1Identity", { enumerable: true, get: function () { return identity_1.AzureKeyVaultSecp256k1Identity; } });
-Object.defineProperty(exports, "AzureKeyVaultSecp256k1IdentityOpts", { enumerable: true, get: function () { return identity_1.AzureKeyVaultSecp256k1IdentityOpts; } });
-var secp256k1_1 = require("./identity/secp256k1");
-Object.defineProperty(exports, "Secp256k1PublicKey", { enumerable: true, get: function () { return secp256k1_1.Secp256k1PublicKey; } });
-//# sourceMappingURL=index.js.map
